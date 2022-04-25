ba77d62a1b6bb9654511328ff9ee7b36a193048b
the-alpaka <98292750+the-alpaka@users.noreply.github.com>
2022-04-25 11:07:56 -0400

    Delete root/defaults/tabby directory

--- root/defaults/tabby/templates/error.php
+++
@@ -1,9 +0,0 @@
-	<div class="container pt-5 pb-3">
-		<div class="row">
-			<div class="col-md-6 offset-md-3">
-				<div class="alert alert-danger" role="alert">
-					<strong>Error</strong> <?php echo $error; ?>
-				</div>
-			</div>
-		</div>
-	</div>
